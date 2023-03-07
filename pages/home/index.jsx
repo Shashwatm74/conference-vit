d15166601cf722a1c35753682f8a963e66a85ce7
Shashwat <shashwat.mishra2022@vitstudent.ac.in>
2023-03-08 01:32:41 +0530

    navbar-fixed-integrated with homepage-shashwat

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import Image from 'next/image';
 import styles from '@/styles/components/homepage/Home.module.scss';
+import formula from '@/public/formula.png';
+import func from '@/public/function.png';
+import meth from '@/public/maths.png';
+import trig from '@/public/trigonometry.png';
+
 
 
 function HomePage() {
@@ -7,6 +13,11 @@ function HomePage() {
 
         <section className={styles.home}>
             <div className={styles.container}>
+                <Image className={styles.image1} src={formula} />
+                <Image className={styles.image2} src={func} />
+                <Image className={styles.image3} src={meth} />
+                <Image className={styles.image4} src={trig} />
+
                 <div className={styles.btnContainer}>
                     <button className={styles.btn}></button>
                 </div>
