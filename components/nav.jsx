9c083d7f1a6f72d86cc70147e1ee9b85299692d6
abhi <abhinavkush2003@gmail.com>
2023-03-08 01:24:44 +0530

    Navbar layout added -abhi

--- components/nav.jsx
+++ components/nav.jsx
@@ -6,35 +6,35 @@ import styles from '@/styles/components/homepage/Nav.module.scss'
 const Nav = () => {
     return (
         <>
-            <div className={styles.nav}>
-                <nav>
-                    <div className="logo">
-                        <Link href="/">
-                            <Image src="/logo.png" width={128} height={77} />
-                        </Link>
-                    </div>
-                    <ul>
-                        <li>
-                            <Link href="/home/about">About</Link>
-                        </li>
-                        <li>
-                            <Link href="/home/speakers">Speakers</Link>
-                        </li>
-                        <li>
-                            <Link href="/home/schedule">Schedule</Link>
-                        </li>
-                        <li>
-                            <Link href="/login">login</Link>
-                        </li>
-                        <li>
-                            <Link href="/home/registrations">Registration</Link>
-                        </li>
-                        <li>
-                            <Link href="/home/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+
+            <nav className={styles.nav}>
+                <div className="logo">
+                    <Link href="/">
+                        <Image src="/logo.png" width={128} height={77} />
+                    </Link>
+                </div>
+                <ul className={styles.nav}>
+                    <li>
+                        <Link href="/home/about">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/home/speakers">Speakers</Link>
+                    </li>
+                    <li>
+                        <Link href="/home/schedule">Schedule</Link>
+                    </li>
+                    <li>
+                        <Link href="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link href="/home/registrations">Registration</Link>
+                    </li>
+                    <li>
+                        <Link href="/home/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+
         </>
     )
 }
