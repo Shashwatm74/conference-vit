31ba183bf0aa8ac0d2285ddaa3040f821d325521
Anshul <anshul.dewangan1@gmail.com>
2023-03-08 18:34:45 +0530

    speaker card layout: AromanticRing

--- components/cards.jsx
+++ components/cards.jsx
@@ -4,15 +4,14 @@ import styles from '@/styles/components/speakerpage/Cards.module.scss';
 function Cards() {
     return (
         <>
-        <div className={styles.card}>
-            <div className={styles.container}>
-                <div className={styles.subcontainer}>Name</div>
-                <div className={styles.subcontainer}>Details</div>
-                <div className={styles.button}>^</div>
+            <div className={styles.card}>
+                <div className={styles.container}>
+                    <div className={styles.namecontainer}>Name</div>
+                    <div className={styles.detailcontainer}>Details</div>
+                </div>
+                <div className={styles.button}></div>
             </div>
-        </div>
         </>
-        
     )
 }
 
