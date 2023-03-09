c9df5af92e82be9df214942b1be1dc945a78c4c9
Anshul <anshul.dewangan1@gmail.com>
2023-03-10 00:11:24 +0530

    Basic structure of footer--anshul

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import styles from '@/styles/footer.module.scss'
-
+import styles from '@/styles/Footer.module.scss'
 
 function Footer() {
     return (
-        <footer>This is Footer</footer>
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                <div className={styles.rows}> 
+                    <div>
+                        <img className={styles.image} src="" alt="Logo" />
+                    </div> 
+                    <div className={styles.link1}>Facebook</div>
+                    <div className={styles.link1}>Instagram</div>
+                    <div className={styles.link1}>Twitter</div>
+                    <div className={styles.link1}>Youtube</div>
+                </div> 
+                <div className={styles.copyright}>
+                    <hr />
+                    &#169; Vellore institute of Technology
+                </div>   
+            </div>
+        </footer>
     )
 }
 
