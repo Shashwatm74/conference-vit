fda356ea711ec40bac2033c5142abedac12eb3f0
Anshul <anshul.dewangan1@gmail.com>
2023-04-19 23:57:27 +0530

    Created and edited files for dashboard -anshul

---
+++ pages/dashboard/index.jsx
@@ -0,0 +1,12 @@
+// This is the dashboard page for the app
+
+ import React from "react";
+
+
+ export default function Dashboard(){
+    return(
+        <>
+       <a href="/dashboard/profile">profile</a> 
+        </>
+    )
+ }
---
+++ pages/dashboard/profile.jsx
@@ -0,0 +1,18 @@
+//ooga booga
+// This is the dashboard profile page for the app
+
+import React from "react";
+import styles from "@/styles/components/dashboard/Profile.module.scss";
+
+export default function Dashboard(){
+   return(
+       <>
+       
+        <section section data-scroll-section className={styles.profile} id="profile">
+            <div className={styles.sectionWrapper}>
+                
+             </div>
+        </section>      
+       </>
+   )
+}
