509ca8a6b00bb535abbff62d7e810dcc4f8997ca
abhi <abhinavkush2003@gmail.com>
2023-04-21 10:50:34 +0530

    working on the dashboard--abhi

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,19 +5,11 @@ function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
-                <div className={styles.rows}> 
-                    <div>
-                        <img className={styles.image} src="" alt="Logo" />
-                    </div>
-                        <div className={styles.link1}>Convenors</div>
-                        <div className={styles.link1}>Register at</div>
-                        <div className={styles.link1}>Designed and Developed by
-                        </div>
-                    </div> 
-                    <div className={styles.copyright}>
-                        <hr />
-                        &#169; Vellore institute of Technology
-                </div>   
+
+                <div className={styles.copyright}>
+                    <hr />
+                    &#169; Vellore institute of Technology
+                </div>
             </div>
         </footer>
     )
--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,12 +1,33 @@
 // This is the dashboard page for the app
 
- import React from "react";
+import React from "react";
+import styles from "@/styles/components/dashboard/Dashboard.module.scss";
+import Link from "next/link";
+const user = "Admin";
 
 
- export default function Dashboard(){
-    return(
+export default function Dashboard() {
+    return (
         <>
-       <a href="/dashboard/profile">profile</a> 
+
+            <aside className={styles.sidebar}>
+                <h1 className={styles.sidebargreeting}>
+                    Hello {user}
+                </h1>
+                <Link href="/dashboard/}">Home</Link>
+                <Link href="/dashboard/}">Announcements</Link>
+                <Link href="/dashboard/}">Submissions</Link>
+                <Link href="/dashboard/}">Results</Link>
+                <Link href="/dashboard/}">Contact us </Link>
+                <Link href="/dashboard/}">Privacy Policy</Link>
+
+                <Link href="/dashboard/profile">profile</Link>
+            </aside>
+            <section className={styles.dashboard}>
+
+                <div className={styles.sectionWrapper}>
+                </div>
+            </section>
         </>
     )
- }
+}
