26b6e833015e9cd2a54a955e657770f24cb09a78
Anshul <anshul.dewangan1@gmail.com>
2023-05-21 12:44:58 +0530

    Created basic Structure of profile and still working on it -anshul

--- pages/dashboard/profile.jsx
+++ pages/dashboard/profile.jsx
@@ -9,9 +9,41 @@ export default function Dashboard(){
        <>
         <section section data-scroll-section className={styles.profile} id="profile">
             <div className={styles.sectionWrapper}>
-                
-                aehq6j25j357j
-            </div>
+                <div className={styles.profileCard}>
+                    ACCOUNT SETTINGS
+                    <div className={styles.photo}>Photo</div>
+                        <div className={styles.headCard}>
+                            <div className={styles.nameHead}>John Wick</div>
+                            <div className={styles.emailHead}>johnwick@gmail.com</div>
+                        </div>
+                        <div className={styles.detailsBox}>
+                            {/* <div className={styles.boxSet}> */}
+                                {/* Username */}
+                                <div className={styles.usernameBox}>@johnwick123</div>
+                            {/* </div> */}
+                            {/* <div className={styles.boxSet} > */}
+                                {/* Full Name */}
+                                <div className={styles.nameBox}>John Wick</div>
+                            {/* </div> */}
+                            {/* <div className={styles.boxSet}> */}
+                                {/* Email Id */}
+                                <div className={styles.emailBox}>johnwick@gmail.com</div>
+                            {/* </div> */}
+                            {/* <div className={styles.boxSet}> */}
+                                {/* Permanent Address */}
+                                <div className={styles.addressBox}>1234 Church Street Rd</div>
+                            {/* </div> */}
+                            {/* <div className={styles.boxSet}> */}
+                                {/* City */}
+                                <div className={styles.cityBox}>New York City</div>
+                            {/* </div> */}
+                            {/* <div className={styles.boxSet}> */}
+                                {/* Pincode */}
+                                <div className={styles.pinBox}>987654</div>
+                            {/* </div> */}
+                        </div>
+                    </div>
+                 </div>
         </section>      
        </>
    )
