3705f1fbc5d5c71aea7410abbc4c86fa7a9cbb6e
abhi <abhinavkush2003@gmail.com>
2023-05-21 19:04:01 +0530

    changed the div to input feild on the profile page--abhi

--- pages/dashboard/profile.jsx
+++ pages/dashboard/profile.jsx
@@ -4,47 +4,43 @@
 import React from "react";
 import styles from "@/styles/components/dashboard/Profile.module.scss";
 
-export default function Dashboard(){
-   return(
-       <>
-        <section section data-scroll-section className={styles.profile} id="profile">
-            <div className={styles.sectionWrapper}>
-                <div className={styles.profileCard}>
-                    ACCOUNT SETTINGS
-                    <div className={styles.photo}>Photo</div>
+export default function Dashboard() {
+    return (
+        <>
+            <section section data-scroll-section className={styles.profile} id="profile">
+                <div className={styles.sectionWrapper}>
+                    <div className={styles.profileCard}>
+                        ACCOUNT SETTINGS
+                        <div className={styles.photo}>Photo</div>
                         <div className={styles.headCard}>
                             <div className={styles.nameHead}>John Wick</div>
                             <div className={styles.emailHead}>johnwick@gmail.com</div>
                         </div>
                         <div className={styles.detailsBox}>
-                            {/* <div className={styles.boxSet}> */}
-                                {/* Username */}
-                                <div className={styles.usernameBox}>@johnwick123</div>
-                            {/* </div> */}
-                            {/* <div className={styles.boxSet} > */}
-                                {/* Full Name */}
-                                <div className={styles.nameBox}>John Wick</div>
+
+                            {/* Full Name */}
+                            <input className={styles.nameBox} placeholder="John Wick" />
                             {/* </div> */}
                             {/* <div className={styles.boxSet}> */}
-                                {/* Email Id */}
-                                <div className={styles.emailBox}>johnwick@gmail.com</div>
+                            {/* Email Id */}
+                            <input className={styles.emailBox} placeholder="johnwick@gmail.com" />
                             {/* </div> */}
                             {/* <div className={styles.boxSet}> */}
-                                {/* Permanent Address */}
-                                <div className={styles.addressBox}>1234 Church Street Rd</div>
+                            {/* Permanent Address */}
+                            <input className={styles.addressBox} placeholder="1234 Church Street Rd" />
                             {/* </div> */}
                             {/* <div className={styles.boxSet}> */}
-                                {/* City */}
-                                <div className={styles.cityBox}>New York City</div>
+                            {/* City */}
+                            <input className={styles.cityBox} placeholder="New York City" />
                             {/* </div> */}
                             {/* <div className={styles.boxSet}> */}
-                                {/* Pincode */}
-                                <div className={styles.pinBox}>987654</div>
+                            {/* Pincode */}
+                            <input className={styles.pinBox} placeholder="987654" />
                             {/* </div> */}
                         </div>
                     </div>
-                 </div>
-        </section>      
-       </>
-   )
+                </div>
+            </section>
+        </>
+    )
 }
